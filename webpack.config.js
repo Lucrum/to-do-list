b573b601d98123104eaabfa807edaabb928388aa
Elton <28992495+Lucrum@users.noreply.github.com>
2023-11-29 17:47:20 -0500

    Add css-loader, style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    index: './src/index.js',
+    index: './src/scripts/index.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -14,4 +14,13 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  mode: 'development'
 };
